bb4c8e65403d09e3575f6734267dad0d7fcffe2f
nhatbarry <nhatbarry@gmail.com>
2024-11-17 15:29:03 +0700

    done login UI

---
+++ booking/src/main/java/App.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nhatb
+ */
+
+import java.awt.EventQueue;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+import view.LoginView;
+
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            EventQueue.invokeLater(new Runnable() {
+                public void run(){
+                    try {
+                        UIManager.setLookAndFeel(new FlatMacLightLaf());
+                    } catch (UnsupportedLookAndFeelException e) {
+                        e.printStackTrace();
+                    }
+                    LoginView view = new LoginView();
+                    view.setVisible(true);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
