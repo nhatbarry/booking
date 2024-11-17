af66527bac6c4fd482ae0168835aa326187c6074
nhatbarry <nhatbarry@gmail.com>
2024-11-17 15:03:05 +0700

    update db

---
+++ booking/src/main/java/data/create_db.sql
@@ -0,0 +1,30 @@
+drop database booking
+go
+
+create database booking
+go
+
+use booking
+
+create table homestay
+(
+	homestay_id int identity(1, 1) primary key
+	, name nvarchar(40)
+	, location nvarchar(120) not null
+	, is_booked bit not null
+	, price int not null
+)
+go
+
+create table people
+(
+	user_id int identity(1, 1) primary key
+	, user_name varchar(40) not null
+	, passwd varchar(40) not null
+	, name nvarchar(40)
+	, phone char(10)
+	, mail char(40)
+	, user_role varchar(10) check (user_role = 'manager' or user_role = 'customer')
+	, homestay_booked int foreign key references homestay(homestay_id)
+)
+go
---
+++ booking/src/main/java/data/insert_db.sql
@@ -0,0 +1,26 @@
+﻿use booking
+go
+
+insert into homestay
+values
+(N'Lii’s Homestay', N'9C, ngách 12/2 Đặng Thai Mai, phường Quảng An, quận Tây Hồ, thành phố Hà Nội', 0, 680000)
+, (N'Nhà Của Bu', N'12A Lý Nam Đế, phường Hàng Mã, quận Hoàn Kiếm, Hà Nội', 0, 650000)
+, (N'Dadia', N'13 Nguyễn Khắc Cần, phường Phan Chu Trinh, quận Hoàn Kiếm, thành phố Hà Nội', 0, 600000)
+, (N'Nhà Bên Rừng', N'Thôn Lâm Trường, Xã Minh Phú, Sóc Sơn, Hà Nội', 0, 1200000)
+, (N'Le Bleu Indie Apartment Hà Nội', N'số 116 Ngõ 10, Đường Nguyên Hồng, Thành Công, Ba Đình, Hà Nội', 0, 300000)
+, (N'Cá Homestay Hà Nội', N'121 Đại La, phường Trương Định, quận Hai Bà Trưng, thành phố Hà Nội', 0, 440000)
+, (N'Veque Villa Homestay Hà Nội', N'21B Xóm Hạ Hồi, phường Trần Hưng Đạo, quận Hoàn Kiếm, thành phố Hà Nội', 0, 500000)
+, (N'Sazi Homestay Hà Nội', N'75 Hàng Bông, phường Hàng Bông, quận Hoàn Kiếm, thành phố Hà Nội', 0, 800000)
+, (N'Mộng Homestay Hà Nội', N' 4/9 ngõ 435 Hồng Hà, phường Phúc Tân, quận Hoàn Kiếm, thành phố Hà Nội', 0, 400000)
+, (N'City Center & Good Security Area ', N'Số 8 ngõ 77 Lê Hồng Phong, Ba Đình, Hà Nội', 0, 500000)
+, (N'An House Homestay Hà Nội', N'208 ngõ 142 Phú Viên, phường Bồ Đề, quận Long Biên, thành phố Hà Nội', 0, 510000)
+, (N'Harmony House Homestay Hà Nội', N'36 Ngõ Phất Lộc, phường Hàng Buồm, quận Hoàn Kiếm, thành phố Hà Nội', 0, 580000)
+, (N'Mei’s Hideaway Homestay Hà Nội', N'2 Nhà Thờ, phường Hàng Trống, quận Hoàn Kiếm, thành phố Hà Nội', 0, 1200000)
+, (N'Sapiens Phố Cổ Hà Nội Homestay', N'68 Hàng Bông, phường Hàng Bông, quận Hoàn Kiếm, thành phố Hà Nội', 0, 599000)
+, (N'Mia Casa by Satori Homestay Hà Nội', N'26 Nam Ngư, phường Cửa Nam, quận Hoàn Kiếm, thành phố Hà Nội', 0, 900000)
+, (N'Gap Yolo Hostel', N'1B Quốc Tử Giám, quận Đống Đa, thành phố Hà Nội', 0, 135000)
+, (N'Airy Budget Homestay', N'Số 8, Ngõ Hàm Long, đường Phan Chu Trinh, Hoàn Kiếm, Hà Nội', 0, 280000)
+, (N'Anchie Homestay', N'số 40 ngõ 93, phố Lý Nam Đế, Phường Cửa Đông, Quận Hoàn Kiếm, Quận Ba Đình, Hà Nội', 0, 500000)
+, (N'Tre House Homestay', N'ngõ 781 Đường Hồng Hà, Chương Dương Độ, Hoàn Kiếm, Hà Nội', 0, 150000)
+, (N'Tuti Homestay Hanoi', N'số 06 Tông Đản, Hoàn Kiếm, Hà Nội', 0, 600000)
+go
